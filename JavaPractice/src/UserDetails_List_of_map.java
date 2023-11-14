f759c376fbfd2f1d18ddd43a06831e9c29263781
Harish Kumar P <harishkp@Harishs-Mac-mini.local>
2023-11-14 23:14:52 +0530

    first commit_1

--- JavaPractice/src/UserDetails_List_of_map.java
+++ JavaPractice/src/UserDetails_List_of_map.java
@@ -4,25 +4,25 @@ import java.util.List;
 import java.util.Map;
 
 public class UserDetails_List_of_map {
-	
-	public static void main(String[] args)
-    {
-		List<Map<String, String>> li = new LinkedList<Map<String, String>>();
-        Map<String, String> map;// = new Map<String, String>();
-        //1st set
-        for (int i=0; i<5; i++) {
-            map = new LinkedHashMap<String, String>();
-            map.put("Name", "Name_"+i);
-            map.put("City", "City_"+i);
-            map.put("Pin", "Pin_"+i);
-            li.add(map);
-        }
-        
-        li.stream().forEach(System.out::println);
 
-        System.out.println(li.get(2).get("Name"));
-        System.out.println(li.get(3).get("City"));
-        System.out.println(li.get(4).get("Pin"));
-    }
+	public static void main(String[] args) 
+	{
+		List<Map<String, String>> li = new LinkedList<Map<String, String>>();
+		Map<String, String> map;// = new Map<String, String>();
+		// 1st set
+		for (int i = 0; i < 5; i++) {
+			map = new LinkedHashMap<String, String>();
+			map.put("Name", "Name_" + i);
+			map.put("City", "City_" + i);
+			map.put("Pin", "Pin_" + i);
+			li.add(map);
+		}
+
+		li.stream().forEach(System.out::println);
+
+		System.out.println(li.get(2).get("Name"));
+		System.out.println(li.get(3).get("City"));
+		System.out.println(li.get(4).get("Pin"));
+	}
 
 }
