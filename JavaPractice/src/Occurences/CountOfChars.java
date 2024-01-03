674df9e0efa884d5c2883743b8333c74265ba18a
Harish Kumar P <harish.avenue1@gmail.com>
2024-01-03 22:23:38 +0530

    Update CountOfChars.java

--- JavaPractice/src/Occurences/CountOfChars.java
+++ JavaPractice/src/Occurences/CountOfChars.java
@@ -13,13 +13,13 @@ public class CountOfChars {
 		System.out.println("Using Stream Filter");
 		String seen = "";
 		for(char c: s.toCharArray()) {
-			// IMP - if present it gives index, else -1
-			if (seen.indexOf(c) != -1) {
+			// to avoid repeating count for same char again, check if already validated
+			if (seen.indexOf(c) != -1) { // IMP - if present it gives index, else -1
 				System.out.println("Already Validated Char " + c + " -> Present at Index -> "+ seen.indexOf(c) + " hence skipping check");
 				continue;
 			}
 			System.out.println("Char -> "+ c +" repeated "+ s.chars().filter(ch -> ch == c).count());
-			seen += c;
+			seen += c; // store to avoid repeatition
 		}
 		
 		System.out.println("------");
