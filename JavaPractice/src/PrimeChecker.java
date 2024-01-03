bbbb6cf2a8e09f0405347e0f0dd59c6d475806d4
Harish Kumar P <harish.avenue1@gmail.com>
2024-01-03 23:14:39 +0530

    Update PrimeChecker.java

--- JavaPractice/src/PrimeChecker.java
+++ JavaPractice/src/PrimeChecker.java
@@ -17,13 +17,13 @@ public class PrimeChecker {
 		else if (num < 2 || num % 2 == 0)
 			return false;
 		else {
-			int sq = (int)Math.sqrt(num);
-			for (int c = 3; c <= sq; c = c + 2) {
+			int sq = (int)Math.sqrt(num);            // get the half value of N, to start multiplying from 1 - N/2 to see if N is multiple in between 1 - N/2
+			for (int c = 3; c <= sq; c = c + 2) {    // 3 because 2 is already checked in 2nd if block, and +2 is becz, even already checked in 2nd if block
 				if (num % c == 0) {
 					return false;
 				}
 			}
 		}
-		return true;
+		return true; // if nothing is validated in above conditions, then its finally true
 	}
 }
