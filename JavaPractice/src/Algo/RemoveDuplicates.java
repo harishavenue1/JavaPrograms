37749536113df75e272c8705f9a2f08ec748a0a1
Harish Kumar P <harish.avenue1@gmail.com>
2024-01-03 22:17:58 +0530

    Update RemoveDuplicates.java

--- JavaPractice/src/Algo/RemoveDuplicates.java
+++ JavaPractice/src/Algo/RemoveDuplicates.java
@@ -8,13 +8,13 @@ public class RemoveDuplicates {
 
 		int slow = 0, fast = 1;
 		while (fast < arr.length) {
-			if (arr[slow] != arr[fast]) {
-				slow++;
-				arr[slow] = arr[fast];
-			}fast=fast+1;
+			if (arr[slow] != arr[fast]) { // check for match only when indexes dont match, so then only slow will get fast index value
+				slow++; // to replace increment slow counter
+				arr[slow] = arr[fast]; 
+			}fast=fast+1; // when things match, then increment fast
 		}
 		
-		// final array
+		// final array // final array is till slow counter, as fast counter might have next duplicate value
 		for (int i=0; i<slow+1;i++) {
 			System.out.println(arr[i]);
 		}
