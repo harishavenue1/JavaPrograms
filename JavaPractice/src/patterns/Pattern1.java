87dc06ba96c90ad888796a2106a2a1edf1dbbada
Harish Kumar P <harish.avenue1@gmail.com>
2024-01-03 22:32:50 +0530

    Update Pattern1.java

--- JavaPractice/src/patterns/Pattern1.java
+++ JavaPractice/src/patterns/Pattern1.java
@@ -8,10 +8,14 @@ public class Pattern1 {
 //		121
 //		12321
 //		1234321
+
+		//Needs twice Math.pow(((Math.pow(n,i)-1)/9),2)
+		//ex:i=1 ---> 10^i - 1 = 9 / 9 = 1^2 = 1
+		//ex:i=2 ---> 10^i - 1 = 99 / 9 = 11^2 = 121
 		
 		int lines=5;
 		for (int i=1; i<=lines; i++) {
-			System.out.println((int)Math.pow(((Math.pow(10, i) - 1)/9),2));
+			System.out.println((int)Math.pow(((Math.pow(10, i) - 1)/9),2));  //ex:i=2 ---> 10^i - 1 = 99 / 9 = 11^2 = 121
 		}
 
 		System.out.println("-------");
@@ -20,6 +24,11 @@ public class Pattern1 {
 //		333
 //		4444
 //		55555
+
+		//Needs Single (Math.pow(n,i)-1)/9 * i
+		//ex:i=1 ---> 10^i - 1 = 9 / 9 = 1*i = 1
+		//ex:i=2 ---> 10^i - 1 = 99 / 9 = 11*i = 22
+		
 		for (int i=1; i<=lines; i++) {
 			System.out.println((int) ((Math.pow(10, i) - 1)/9) * i);
 		}
