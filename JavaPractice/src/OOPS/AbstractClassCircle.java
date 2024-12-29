9585be8845579af0bc20188f69d16356f3a18ac0
Harish Kumar P <harishkp@Harishs-Mini.lan>
2024-12-29 11:21:57 +0530

    OOPS...

---
+++ JavaPractice/src/OOPS/AbstractClassCircle.java
@@ -0,0 +1,23 @@
+package OOPS;
+
+public class AbstractClassCircle extends Shape {
+	double radius;
+
+	public AbstractClassCircle(String color, double radius) {
+
+		// calling Shape constructor
+		super(color);
+		System.out.println("Circle constructor called");
+		this.radius = radius;
+	}
+
+	@Override
+	double area() {
+		return Math.PI * Math.pow(radius, 2);
+	}
+
+	@Override
+	public String toString() {
+		return "Circle color is " + super.getColor() + "and area is : " + area();
+	}
+}
---
+++ JavaPractice/src/OOPS/TestingOOPS.java
@@ -0,0 +1,14 @@
+package OOPS;
+
+public class TestingOOPS {
+	public static void main(String[] args) {
+		Shape s1 = new AbstractClassCircle("Red", 2.2);
+		Shape s2 = new Rectangle("Yellow", 2, 4);
+
+		// if I try to know implementation of below methods I cant see -> this is abstraction
+		// because I have ref of abstract class, but implementation logic is in child class
+		// child class implementation wont be seen, as ref is to main parent class
+		System.out.println(s1.toString()); 
+		System.out.println(s2.toString());
+	}
+}
